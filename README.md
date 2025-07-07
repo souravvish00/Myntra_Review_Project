4e95a831b5cf442ec4ced6ca82c6950256c4b1c9
souravvish00 <sos45545445@gmail.com>
2025-07-07 21:23:03 +0530

    this is my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Myntra Review Scraper project
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='sourav vishwakrma',
+    author_email='sourav9628@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
+
