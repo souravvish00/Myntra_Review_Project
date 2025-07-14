ba2fe33010376f3fa88196ebd84cb0bdb99aabb7
souravvish00 <sos45545445@gmail.com>
2025-07-14 22:49:12 +0530

    Final Commit

---
+++ src/constant/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://Sourav:Sourav0987@cluster0.wwq8ftz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
